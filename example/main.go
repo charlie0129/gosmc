15314df7e83a2e4379ca9c674342193d2086c79d
Charlie Chiang <charlie_c_0129@outlook.com>
2023-06-10 00:23:26 +0800

    refine example
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- example/main.go
+++ example/main.go
@@ -3,62 +3,60 @@ package main
 import (
 	"flag"
 	"fmt"
-	"os"
+	"log"
 	"strconv"
 
 	"github.com/charlie0129/gosmc"
 )
 
 func main() {
+	key := ""
+	val := ""
+	flag.StringVar(&key, "k", "", "SMC key to read/write")
+	flag.StringVar(&val, "v", "", "Value to write to SMC key. If not specified, read value from SMC key")
+	flag.Parse()
+
+	if key == "" {
+		log.Fatal("You must specify a key to read/write with -k")
+	}
+
 	c := gosmc.New()
 
 	// Open connection to SMC.
 	err := c.Open()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	// Close connection once we are done.
 	defer c.Close()
 
-	key := ""
-	val := ""
-	flag.StringVar(&key, "k", "", "SMC key to read/write")
-	flag.StringVar(&val, "v", "", "Value to write to SMC key")
-	flag.Parse()
-
-	if key == "" {
-		fmt.Printf("You must specify a key to read/write with -k\n")
-		os.Exit(1)
-	}
-
 	// Read value from SMC key.
 	if val == "" {
 		v, err := c.Read(key)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
-		fmt.Printf("%s: %v\n", v.Key, v.Bytes)
+		fmt.Printf("%v\n", v.Bytes)
 		return
 	}
 
 	// Write value to SMC key.
 	if len(val)%2 != 0 {
-		fmt.Printf("Value must be hex encoded\n")
-		os.Exit(1)
+		log.Fatal("Value must be hex encoded\n")
 	}
 
 	b := make([]byte, len(val)/2)
 	for i := 0; i < len(val); i += 2 {
 		v, err := strconv.ParseUint(val[i:i+2], 16, 8)
 		if err != nil {
-			panic(err)
+			log.Fatal(err)
 		}
 		b[i/2] = byte(v)
 	}
 
 	err = c.Write(key, b)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
